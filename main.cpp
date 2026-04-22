7fe27aa838892402a492039db322851834a80fea
moretto08 <mateusmoretto@disroot.org>
2026-04-22 13:34:35 -0300

    added license files/header

--- main.cpp
+++ main.cpp
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2026 Mateus
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ */
+
 #include <gtkmm.h>
 #include <gtk-layer-shell.h>
 #include <iostream>
